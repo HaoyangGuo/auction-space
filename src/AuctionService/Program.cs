ced9efbaaa88833dc509ccfdba3f42934591bf31
Haoyang Guo <82100936+HaoyangGuo@users.noreply.github.com>
2025-02-10 23:50:35 -0800

    feat(auction-svc): clean up webapi template project

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,17 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt => {
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
